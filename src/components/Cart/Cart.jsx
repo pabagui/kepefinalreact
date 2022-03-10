a471728b2326ebce67e8a62cab62ef494d5048b5
pabagui <aguila.pablo@gmail.com>
2022-03-10 00:59:48 -0300

    arreglado comprador de formulario para firestore

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,7 +1,6 @@
-import { useState } from 'react'
+
 import { Link } from 'react-router-dom';
 import { useCartContext } from '../../context/CartContext';
-import { getFirestore, addDoc, collection, query, documentId, writeBatch, getDocs, where } from 'firebase/firestore';
 import Button from 'react-bootstrap/Button'
 import Form from '../Form/Form';
 import CartProducts from '../CartProducts/CartProducts';
@@ -9,73 +8,9 @@ import CartProducts from '../CartProducts/CartProducts';
 
 const Cart = () => {
 
- 
-  const  [orderId, setOrderId] = useState('')  
-
-  const  [dataForm, setDataForm] = useState({ 
-            name: '',
-            phone: '',
-            email: '', 
-})
-
   const { cartList, clear, totalCart } = useCartContext()
-
-  const buyOrder = async (e) => {
-    e.preventDefault()
-
-    let order = {}
-
-    order.buyer = dataForm
-    order.total = totalCart()
-
-    order.items = cartList.map(cartItem => {
-      const id = cartItem.item.id
-      const name = cartItem.item.name
-      const price = cartItem.item.price * cartItem.quantity
-      const quantity = cartItem.quantity
-
-      return {
-        id,
-        name,
-        price,
-        quantity
-      }
-
-    })
-
-      const db = getFirestore()
-      const ordersCollection = collection(db, 'orders')
-      await addDoc(ordersCollection, order)
-        .then(resp => setOrderId(resp.id))
-
-      const queryCollection = collection(db, 'items')
-
-      const queryUpdateStock = query( 
-          queryCollection,
-          where( documentId(), 'in', cartList.map(it => it.item.id) ) 
-      )
-      const batch = writeBatch(db)
-
-      await  getDocs(queryUpdateStock) 
-          .then(resp => resp.docs.forEach(res => batch.update(res.ref, {
-              stock: res.data().stock - cartList.find(item => item.item.id === res.id).quantity
-            })
-          ))
-          .catch(err => console.log(err))
-
-          .finally(() => {
-                  setDataForm({ 
-                    name: '',
-                    phone: '',
-                    email: '',   
-                })
-          })        
-      batch.commit() 
-  }
-
-    return <div className="container w-50"> 
-              {orderId !== '' && `El número de tu pedido es ${orderId} , gracias por comprar en K'epe Bags`}
-              <br/>            
+ 
+    return <div className="container w-50">        
               {cartList.length !== 0 ? 
                 <>
                   { cartList.map( (product) => 
@@ -91,7 +26,7 @@ const Cart = () => {
                   <Button variant="dark" onClick={clear}>Vaciar canasta</Button>
                   <br/>
                   <br/>
-                  <Form buyOrder={buyOrder} />
+                  <Form />
                   <br/>
                   <Link to='/'>
                   <Button variant="dark">Volver a la tienda</Button>
@@ -108,6 +43,5 @@ const Cart = () => {
               }
             </div>
 };
-
 
 export default Cart;
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,14 +1,72 @@
 import { useState } from 'react'
+import { useCartContext } from '../../context/CartContext';
+import { getFirestore, addDoc, collection, query, documentId, writeBatch, getDocs, where } from 'firebase/firestore';
 import Button from 'react-bootstrap/Button'
 
-
-const Form = ({ buyOrder }) => {
+const Form = () => {
     
+    const  [orderId, setOrderId] = useState('')  
     const  [dataForm, setDataForm] = useState({
         name: '',
         phone: '',
         email: '', 
     })
+
+    const { cartList, clear, totalCart } = useCartContext()
+
+    const buyOrder = async (e) => {
+        e.preventDefault()
+    
+        let order = {}
+    
+        order.buyer = dataForm
+        order.total = totalCart()
+    
+        order.items = cartList.map(cartItem => {
+          const id = cartItem.item.id
+          const name = cartItem.item.name
+          const price = cartItem.item.price * cartItem.quantity
+          const quantity = cartItem.quantity
+    
+          return {
+            id,
+            name,
+            price,
+            quantity
+          }
+        })
+    
+          const db = getFirestore()
+          const ordersCollection = collection(db, 'orders')
+          await addDoc(ordersCollection, order)
+            .then(resp => setOrderId(resp.id))
+    
+          const queryCollection = collection(db, 'items')
+    
+          const queryUpdateStock = query( 
+              queryCollection,
+              where( documentId(), 'in', cartList.map(it => it.item.id) ) 
+          )
+          const batch = writeBatch(db)
+    
+          await  getDocs(queryUpdateStock) 
+              .then(resp => resp.docs.forEach(res => batch.update(res.ref, {
+                  stock: res.data().stock - cartList.find(item => item.item.id === res.id).quantity
+                })
+              ))
+              .catch(err => console.log(err))
+    
+              .finally(() => {
+                      setDataForm({ 
+                        name: '',
+                        phone: '',
+                        email: '',   
+                    })
+                    clear()
+                    alert(`Tu pedido ha sido enviado. Gracias por comprar en K'epe Bags`)
+              })        
+          batch.commit() 
+      }
 
     const handleChange = (event) => {      
         setDataForm({ 
@@ -44,18 +102,10 @@ const Form = ({ buyOrder }) => {
                         onChange={handleChange}
                         value={dataForm.email}
                     />
-                    <input 
-                        type='email' 
-                        name='validateEmail'
-                        placeholder='repetir email' 
-                        onChange={handleChange}
-                        value={dataForm.validateEmail}
-                    />
                     <br/>
                     <br/>
                     <Button variant="dark" onClick={buyOrder}>Generar orden de compra</Button>
         </form>
-
     </div>
   )
 }
